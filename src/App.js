ef94df2d01ad09292f5ae73e2ec75c4c919f94a4
sevdas <sshukrieva@gmail.com>
2021-07-12 11:18:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Speak Easy Sprint 1</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <a
           className="App-link"
           href="https://reactjs.org"
