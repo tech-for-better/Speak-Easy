bb41efff3cfabae9e29543c0ba74f49b08dbe1dc
sevdas <sshukrieva@gmail.com>
2021-08-01 20:16:44 +0100

    fix production error

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { client } from "../lib/api";
 import { useSpeechSynthesis } from "react-speech-kit";
 import Header from "../components/Header";
